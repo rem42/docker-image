13a42cf7a2507ef694ad653f45c5813012a255e0
rem42 <me@remy.ovh>
2022-02-08 20:42:24 +0100

    update versions

--- scripts/build_one.sh
+++ scripts/build_one.sh
@@ -8,6 +8,6 @@ path="$RENDER_DIR/php.$version.$variant.Dockerfile"
 if [ -f "$path" ]
 then
   tagName="rem42/circleci-docker-php:$version-$variant"
-  docker build -t "$tagName-latest" -t "$tagName-$(date +'%Y%m%d')" --label "$tagName-$(date +'%Y%m%d')" - < "$path"
+  docker build --pull -t "$tagName-latest" -t "$tagName-$(date +'%Y%m%d')" --label "$tagName-$(date +'%Y%m%d')" - < "$path"
   docker push rem42/circleci-docker-php --all-tags
 fi
